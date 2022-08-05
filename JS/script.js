7b6c2d671e11215ed3bff2d58ec435aab2a90870
Safaa Magdy <71824692+safaamagdy44@users.noreply.github.com>
2022-08-05 20:35:52 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+doctors=[
+    {name:"dr:mohammad magdy" ,twitter:"mohammad magdy@twitter",google:"mohammad magdy@google" , image:"doctor1.jpg"},
+    {name:"dr:mona magdy" ,twitter:"mona magdy@twitter",google:"mona magdy@google" , image:"doctor2.jpg"},
+    {name:"dr:alia mohammad" ,twitter:"alia mohammad@twitter",google:"alia mohammad@google" , image:"doctor3.jpg"},
+    {name:"dr:al dchhmmad" ,twitter:"al dchhmmad@twitter",google:"al dchhmmad@google" , image:"doctor4.jpg"},
+    {name:"dr:dkckdmdh mmad" ,twitter:"dkckdmdh mmad@twitter",google:"dkckdmdh mmad@google" , image:"doctor5.jpg"},
+    {name:"dr:xnbsc mohammad" ,twitter:"xnbsc mohammad@twitter",google:"xnbsc mohammad@google" , image:"doctor6.jpg"}
+];
+clients=[
+{name:"MR:mohammad abdelrahman" , 
+opinion:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. maiores eum? Possimus, maxime?" , image:"p1.jpg"},
+{name:"MrR:souaad magdy" , 
+opinion:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. maiores eum? Possimus, maxime?Lorem ipsum dolor sit, amet consectetur adipisicing elit. maiores eum? Possimus, maxime?" , 
+image:"p2.jpg"},
+{name:"MR:khalid awad" , 
+opinion:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. maiores eum?s eum? Possimus, maxime?" , image:"p3.jpg"},
+{name:"MRS:fawzia abd el-motagally" ,
+ opinion:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. maiores eum? Possimus, maxime?" , image:"p4.jpg"}
+];
